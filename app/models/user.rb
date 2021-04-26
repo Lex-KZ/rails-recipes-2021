128a1bf1088aab4af9a6adea85b57835428f6c18
Michael Bamford <michael@michaelbamford.com>
2021-04-27 09:52:58 +1000

    Fix initialization of User model with callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
     validates :skill_level, inclusion: { in: 1..5 }
     validates :skill_level, numericality: { only_integer: true }
 
-    def initialize(args)
-        super
-        self.skill_level = 1
+    after_initialize do |user|
+        user.skill_level ||= 1
     end
 end
