e963ea8d433adcdf886752d284c951c99faf198b
Michael Bamford <michael@michaelbamford.com>
2021-04-28 10:31:39 +1000

    Add User foreign key to Recipe

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     validates :skill_level, inclusion: { in: 1..5 }
     validates :skill_level, numericality: { only_integer: true }
 
+    has_many :recipes
+
     after_initialize do |user|
         user.skill_level ||= 1
     end
